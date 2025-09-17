d1a837c23b46245b953afa559336f2067a8c2892
ShivangiSingh07 <shivangisingh7@outlook.com>
2025-09-18 02:43:42 +0530

    Create Day1_1.c

---
+++ Day1_1.c
@@ -0,0 +1,17 @@
+/* ⭐ Program to add two numbers ⭐ */
+
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Input two numbers
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Output their sum directly
+    printf("Sum = %d\n", a + b);
+
+    return 0;
+}
