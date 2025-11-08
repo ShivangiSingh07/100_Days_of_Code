463cb518fc63e7819af48644f921621c29507bc5
Shivangi Singh <shivangisingh7@outlook.com>
2025-11-08 10:26:41 +0530

    Create Day17_1.c

---
+++ Day17_1.c
@@ -0,0 +1,38 @@
+Q33: Write a program to check if a number is an Armstrong number.
+
+/*
+Sample Test Cases:
+Input 1:
+153
+Output 1:
+Armstrong
+
+Input 2:
+123
+Output 2:
+Not Armstrong
+
+*/
+
+#include<stdio.h>
+int main (){
+    int n ,temp,x,amp=0;
+    printf("Enter any number :");
+    scanf("%d",&n);
+    temp=n;
+    while(n!=0){
+        x=n%10;
+        amp=amp+x*x*x;
+        n=n/10;
+    }
+    
+    if(temp==amp){
+        printf("Armstrong");
+    }
+    else{
+        printf("Not Armstrong");
+    }
+    return 0;
+    
+
+}
