6b542cc1130ca4c137ceba1ed485c72256204e4a
Shivangi Singh <shivangisingh7@outlook.com>
2025-12-02 12:07:00 +0530

    Create Day58_1.c

---
+++ Day58_1.c
@@ -0,0 +1,52 @@
+Q108: Write a Program to take an integer array nums. Print an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i]. The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+/*
+Sample Test Cases:
+Input 1:
+nums = [1,2,3,4]
+Output 1:
+[24,12,8,6]
+
+Input 2:
+nums = [-1,1,0,-3,3]
+Output 2:
+[0,0,9,0,0]
+
+*/
+
+  #include <stdio.h>
+
+void productExceptSelf(int nums[], int n) {
+    int answer[n];
+
+    for (int i = 0; i < n; i++) {
+        int product = 1;
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                product *= nums[j];
+            }
+        }
+        answer[i] = product;
+    }
+
+    printf("[");
+    for (int i = 0; i < n; i++) {
+        if (i == n - 1)
+            printf("%d", answer[i]);
+        else
+            printf("%d,", answer[i]);
+    }
+    printf("]\n");
+}
+
+int main() {
+    int nums1[] = {1, 2, 3, 4};
+    int n1 = sizeof(nums1) / sizeof(nums1[0]);
+    productExceptSelf(nums1, n1);
+
+    int nums2[] = {-1, 1, 0, -3, 3};
+    int n2 = sizeof(nums2) / sizeof(nums2[0]);
+    productExceptSelf(nums2, n2);
+
+    return 0;
+}
